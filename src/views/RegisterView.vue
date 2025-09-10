3887739fd6ed8ac033150e5606604a530d4631fa
Lucas emanoel <lucasemanoel625@gmail.com>
2025-09-10 20:13:44 -0300

    feat: Implementa a lógica para capturar dados do formulário

--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -6,20 +6,52 @@
     <div class="columns is-centered">
       <div class="column is-half">
         <p>Nome</p>
-        <input type="text" placeholder="Nome do usuário" class="input" />
+        <input
+          type="text"
+          placeholder="Nome do usuário"
+          class="input"
+          v-model="name"
+        />
         <p>E-mail</p>
-        <input type="email" placeholder="email@email.com" class="input" />
+        <input
+          type="email"
+          placeholder="email@email.com"
+          class="input"
+          v-model="email"
+        />
         <p>Senha</p>
-        <input type="password" placeholder="******" class="input" />
-        <hr>
-        <button class="button is-primary is-dark">Cadastrar</button>
+        <input
+          type="password"
+          placeholder="******"
+          class="input"
+          v-model="password"
+        />
+        <hr />
+        <button class="button is-primary is-dark" @click="register">
+          Cadastrar
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      name: "",
+      password: "",
+      email: "",
+    };
+  },
+  methods: {
+    register() {
+      console.log(this.name);
+      console.log(this.password);
+      console.log(this.email);
+    },
+  },
+};
 </script>
 
 <style scoped></style>
