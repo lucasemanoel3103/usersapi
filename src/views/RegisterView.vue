bb447c81d1157b14c732ce6711031824fb60b183
Lucas emanoel <lucasemanoel625@gmail.com>
2025-09-10 20:05:18 -0300

    feat: Implementa front-end do formulário de cadastro de usuário

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Registro de usuário!</h2>
+    <hr />
+
+    <div class="columns is-centered">
+      <div class="column is-half">
+        <p>Nome</p>
+        <input type="text" placeholder="Nome do usuário" class="input" />
+        <p>E-mail</p>
+        <input type="email" placeholder="email@email.com" class="input" />
+        <p>Senha</p>
+        <input type="password" placeholder="******" class="input" />
+        <hr>
+        <button class="button is-primary is-dark">Cadastrar</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
